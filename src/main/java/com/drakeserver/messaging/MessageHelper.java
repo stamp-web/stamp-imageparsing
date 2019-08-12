057fd2a6498f871745abe9ced8f0d91dabc3dceb
jadrake75 <jadrake75@gmail.com>
2019-08-12 02:11:47 -0400

    Minor update to trigger

--- src/main/java/com/drakeserver/messaging/MessageHelper.java
+++ src/main/java/com/drakeserver/messaging/MessageHelper.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class MessageHelper {
 
