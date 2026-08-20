af5c11ccba889b9df1e53a932b679f6e49f445f2
jadrake75 <jadrake75@gmail.com>
2026-08-20 13:29:00 -0400

    Fix broken building issue

--- web-app/aurelia_project/tasks/stage.js
+++ web-app/aurelia_project/tasks/stage.js
@@ -1,10 +1,11 @@
 import gulp from 'gulp';
-import project from "../aurelia";
+import project from '../aurelia.json';
 const fs   = require('fs');
 
 let stage = () => {
-    console.log('Copying library files from Java Project')
+    console.log('Copying library files from Java Project');
     fs.copyFileSync(`../target/${project.paths.libFile}`, `${project.paths.stage}/${project.paths.libFile}`);
+    return Promise.resolve();
 };
 
 export { stage as default };
